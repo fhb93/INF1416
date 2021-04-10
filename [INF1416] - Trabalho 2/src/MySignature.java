4d10612fcfe65215c8ac22deb856defbcef223c1
Nathalia <nathi.mariz.inacio@gmail.com>
2021-04-10 14:36:25 -0300

    Creating folder for project 2 with classes templates

---
+++ [INF1416] - Trabalho 2/src/MySignature.java
@@ -0,0 +1,66 @@
+import java.security.*;
+import java.util.Arrays;
+
+import javax.crypto.*;
+
+/**
+ * INF1416 - Trabalho 2
+ *  Dupla:
+ * 		Felipe Holanda Bezerra - Matrícula:
+ * 		Nathalia Mariz de Almeida Salgado Inácio - Matrícula:
+*/
+
+
+public class MySignature {
+	
+	private Cipher cipherPattern; 
+	private MessageDigest messageDig;
+	
+	//Métodos da classe MySignature - getInstance, initSign, update, sign, initVerify e verify
+	
+	
+	//Método getInstance() -> Instancia a classe MySignature e gera as chaves RSA
+	public MySignature() throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException {
+		this.cipherPattern = Cipher.getInstance("RSA");
+		this.messageDig = MessageDigest.getInstance("MD5");	
+	}
+	
+	//Método initSign() -> Inicializa a assinatura digital
+	public void initSign(PrivateKey key) throws InvalidKeyException {		
+		this.cipherPattern.init(Cipher.ENCRYPT_MODE, key);
+	}
+	  
+	//Método update() -> Pega o resumo de mensagem 
+	public void update(byte[] data) {
+	    this.messageDig.update(data);
+	}
+	
+	//Método sign() -> Assina com chave privada
+	public byte[] sign() throws IllegalBlockSizeException, BadPaddingException {
+		return this.cipherPattern.doFinal(this.messageDig.digest());
+	}
+	  
+	//Método initVerify() -> Inicializa a verificação da assinatura com a chave pública
+	public void initVerify(PublicKey key) throws InvalidKeyException {
+		this.cipherPattern.init(Cipher.DECRYPT_MODE, key);
+	}
+	
+	//Método verify() -> Checa se a assinatura foi validada
+	public boolean verify(byte[] signature) throws IllegalBlockSizeException, BadPaddingException {
+		return Arrays.equals(this.messageDig.digest(), cipherPattern.doFinal(signature));
+	}
+	  
+	
+	//Método auxiliar - Formata a assinatura digital para hexadecimal e a imprime
+	public static void hexFormat(byte[] bytes) {
+		  
+		StringBuffer digitalSignature = new StringBuffer();
+	    for(int i = 0; i < bytes.length; i++) {
+	    	String hex = Integer.toHexString(0x0100 + (bytes[i] & 0x00FF)).substring(1);
+	    	digitalSignature.append((hex.length() < 2 ? "0" : "") + hex);
+	    }
+	
+	    System.out.println( digitalSignature.toString() );
+	}
+
+}
---
+++ [INF1416] - Trabalho 2/src/MySignatureTest.java
@@ -0,0 +1,22 @@
+/**
+ * INF1416 - Trabalho 2
+ *  Dupla:
+ * 		Felipe Holanda Bezerra - Matrícula:
+ * 		Nathalia Mariz de Almeida Salgado Inácio - Matrícula:
+*/
+
+public class MySignatureTest {
+
+	public static void main(String[] args) {
+		//TODO:
+		/**
+		 * Receber a string e o padrao da assinatura na linha de comando
+		 * Gerar o par de chaves RSA
+		 * Instanciar e usar os métodos da classe MySignature para assinar o texto plano com chave privada
+		 * Verificar a assinatura com a chave publica no padrão solicitado
+		 * Imprimir, na saída padrão, todos os passos executados durante a geração e verificação da assinatura digital
+		 * Imprimir, na saída padrão, o resumo de mensagem (digest) e a assinatura digital no formato hexadecimal
+		*/
+	}
+
+}
